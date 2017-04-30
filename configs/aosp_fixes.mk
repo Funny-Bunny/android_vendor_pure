65e72587155ecafdba3e25e11ecd731a188fec84
Funny-Bunny <dragos281993@gmail.com>
2017-04-30 19:01:37 +0300

    mako: fix Keyboard swyping. Signed-off-by: spezi77 <spezi77@gmx.com>
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -26,7 +26,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.rotation_locked=true
 
 # Camera Effects for devices without a vendor partition
-ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
+ifneq ($(filter mako,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES +=  \
     vendor/pure/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/pure/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
